5c426bcd2c1610e092a23051cf401bf5ee11021b
kelvintavarez10029 <113301429+kelvintavarez10029@users.noreply.github.com>
2025-03-08 09:54:17 -0500

    frist-attempt

--- constructor-exercise.js
+++ constructor-exercise.js
@@ -1,1 +1,39 @@
 /* Please read the instructions located in the 1-constructor-exercise-readme.md file and complete your code below */ 
+function Book(title, author, year, genre) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.genre = genre;
+}
+  Book.prototype.getSummary = function(){
+    return `${this.title} was written by ${this.author} in ${this.year}. It is a ${this.genre} book.`
+  }
+
+ Book.prototype.isClassic = function() {
+    let currentYear = new Date().getFullYear();
+    return (currentYear - this.year >= 50) ? `${this.title} is a classic.` : `${this.title} is not a classic.`;
+};
+
+  Book.prototype.updateYear = function(newYear){
+    this.year = newYear;
+    return `The year of ${this.title} has been updated to ${this.year}.`
+  }
+  Book.prototype.changeGenre = function(newGenre) {
+    this.genre = newGenre;
+    return `Genre updated to ${newGenre}`;
+};
+
+let book1 = new Book("To Kill a Mockingbird", "Harper Lee", 1960, "Fiction");
+
+console.log(book1.getSummary()); 
+
+console.log(book1.isClassic()); 
+
+console.log(book1.updateYear(2000)); 
+
+console.log(book1.changeGenre("Historical Fiction")); 
+
+  
+
+  
+
